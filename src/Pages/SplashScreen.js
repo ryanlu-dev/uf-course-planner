667e8e09525a877a46e3faecdb2376dfec97bdce
ryanlu-dev <ryan@ryanlu.dev>
2024-11-10 14:28:59 -0500

    add skeleton to splash page

--- src/Pages/SplashScreen.js
+++ src/Pages/SplashScreen.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import './Styles/HomePage.css';
+import './Styles/SplashScreen.css';
+import { useNavigate } from "react-router-dom";
 
 const SplashScreen = () => {
+    const navigate = useNavigate(); 
     return (
-        <div className="home-container">
-            <header className="home-header">
-                <h1>Welcome to the <span className="highlight">UF Course Planner</span></h1>
-                <p>Your personalized platform for academic planning and progress tracking.</p>
-            </header>
-
-            <main className="home-main">
+        <div className="splash-page">
+            <div className="splash-container-1">
+                <header className="splash-header">
+                    <h1>Welcome to the <span className="highlight">UF Course Planner</span></h1>
+                    <p>Your personalized platform for academic planning and progress tracking.</p>
+                </header>
+                <main className="splash-main">
+                    <div className="login-button">
+                        <button onClick={() => { navigate("/login"); }}>
+                            Login
+                        </button>
+                    </div>
+                </main>
+            </div>
+            <div className="splash-container-2">
                 <h1>
-                    <a href="/login"> Please log in! </a>
+                    Item 1
                 </h1>
-            </main>
-
+            </div>
+            <div className="splash-container-2">
+                <h1>
+                    Item 2
+                </h1>
+            </div>
+            <div className="splash-container-2">
+                <h1>
+                    Item 3
+                </h1>
+            </div>
         </div>
     );
 };

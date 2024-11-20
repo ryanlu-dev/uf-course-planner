a705da00a16290afbaacad22f83670a8574a5fac
unknown <connor.curcio16@gmail.com>
2024-11-19 20:17:26 -0500

    update

--- src/Pages/Courses.js
+++ src/Pages/Courses.js
@@ -15,13 +15,12 @@ function Courses() {
     const fetchAllCourses = async () => {
         setLoading(true);
         setError(null);
-        let endpoint = '/data-api/rest/courses';
         let hasNextPage = true;
 
         try {
             let accumulatedCourses = [];
             while (hasNextPage) {
-                const response = await fetch(endpoint);
+                const response = await fetch('/data-api/rest/courses');
                 const data = await response.json();
                 console.log('Fetched data:', data);
 

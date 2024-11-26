7abe62f4b8dbf9e276d298e77f26e6a294ad29e0
ryanlu-dev <ryan@ryanlu.dev>
2024-11-26 16:19:22 -0500

    enable tls

--- api/getUsers/index.js
+++ api/getUsers/index.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 const fs = require('fs');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 module.exports = async function (context, req) {
     var client = new Client({host:"ufcourseplannerdb.postgres.database.azure.com", user:"bytesquad", password:process.env.DB_PASS, database:"ufcourseplanner", port:5432, ssl:{ca:fs.readFileSync("Microsoft RSA Root Certificate Authority 2017.crt")}});
     try {

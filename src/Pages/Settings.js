cefbe6b77810668c7981ae7b4c7c6875aace4d52
ryanlu-dev <ryan@ryanlu.dev>
2024-11-25 23:31:13 -0500

    test database retrieval with js

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -2,15 +2,43 @@ import React from 'react';
 import './Styles/Settings.css'; // Optional for custom styles
 
 const Settings = () => {
+    async function getUserInfo(azure_id) {
+        const endpoint = `/data-api/rest/users/azure_id`;
+        const response = await fetch(`${endpoint}/${azure_id}`);
+        const result = await response.json();
+        return result.value;
+    }
+
+    async function getMajorInfo(major_id) {
+        const endpoint = `/data-api/rest/majors/major_id`;
+        const response = await fetch(`${endpoint}/${major_id}`);
+        const result = await response.json();
+        return result.value;
+    }
+
     const azure_id = sessionStorage.getItem("azure_id");
     const email = sessionStorage.getItem("email");
-    console.log("Azure ID of user: ", azure_id);
-    console.log("Email of user: ", email);
+    const userInfo = getUserInfo(azure_id);
+    const majorInfo = getMajorInfo(userInfo.major_id);
 
     return (
         <div>
             <h2>Settings</h2>
             <p>Here you can view and manage your settings.</p>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Major</th>
+                    <th>Semester</th>
+                </tr>
+                <tr>
+                    <td>{userInfo.name}</td>
+                    <td>{email}</td>
+                    <td>{majorInfo.name}</td>
+                    <td>{userInfo.current_semester}</td>
+                </tr>
+            </table>
         </div>
     );
 };

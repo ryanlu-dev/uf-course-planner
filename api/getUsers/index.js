d9387cf602d18400cf4b57f95a31af62614c874c
ryanlu-dev <ryan@ryanlu.dev>
2024-11-26 19:16:24 -0500

    attempt cert merge

--- api/getUsers/index.js
+++ api/getUsers/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 module.exports = async function (context, req) {
-    var client = new Client({host:"ufcourseplannerdb.postgres.database.azure.com", user:"bytesquad", password:process.env.DB_PASS, database:"ufcourseplanner", port:5432, ssl:{ca:fs.readFileSync("Microsoft RSA Root Certificate Authority 2017.crt")}});
+    var client = new Client({host:"ufcourseplannerdb.postgres.database.azure.com", user:"bytesquad", password:process.env.DB_PASS, database:"ufcourseplanner", port:5432, ssl:{ca:fs.readFileSync("mergedcert.pem").toString()}});
     try {
         await client.connect();
         const result = await client.query('SELECT * FROM users');

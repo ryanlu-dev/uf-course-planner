01ae3b71d1192110278b5c791d56ec705969c340
ryanlu-dev <ryan@ryanlu.dev>
2024-12-02 04:29:41 -0500

    remove dangling apostrophes in sql query

--- api/updateProfile/index.js
+++ api/updateProfile/index.js
@@ -10,8 +10,8 @@ module.exports = async function (context, req) {
         const m_id = req.body.major_id;
         const curr_sem = req.body.current_semester;
         const query = `
-        INSERT INTO users (name, major_id, current_semester, azure_id) VALUES ($1, $2, $3, $4') ON CONFLICT (azure_id) DO UPDATE SET name = EXCLUDED.name, major_id = EXCLUDED.major_id, current_semester = EXCLUDED.current_semester WHERE users.azure_id = EXCLUDED.azure_id;
-        SELECT users.name, majors.name, users.current_semester FROM users LEFT JOIN majors ON majors.major_id = users.major_id WHERE azure_id = $4'`
+        INSERT INTO users (name, major_id, current_semester, azure_id) VALUES ($1, $2, $3, $4) ON CONFLICT (azure_id) DO UPDATE SET name = EXCLUDED.name, major_id = EXCLUDED.major_id, current_semester = EXCLUDED.current_semester WHERE users.azure_id = EXCLUDED.azure_id;
+        SELECT users.name, majors.name, users.current_semester FROM users LEFT JOIN majors ON majors.major_id = users.major_id WHERE azure_id = $4`
         const result = await client.query(query, [name, m_id, curr_sem, a_id]);
         context.res = {
             status: 200,

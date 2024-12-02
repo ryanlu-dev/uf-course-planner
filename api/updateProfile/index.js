72bd0cec07f32d241dc2174fdef8b658dd2c1e48
ryanlu-dev <ryan@ryanlu.dev>
2024-12-02 04:37:30 -0500

    separate query into two prepared statements

--- api/updateProfile/index.js
+++ api/updateProfile/index.js
@@ -9,13 +9,13 @@ module.exports = async function (context, req) {
         const name = req.body.name;
         const m_id = req.body.major_id;
         const curr_sem = req.body.current_semester;
-        const query = `
-        INSERT INTO users (name, major_id, current_semester, azure_id) VALUES ($1, $2, $3, $4) ON CONFLICT (azure_id) DO UPDATE SET name = EXCLUDED.name, major_id = EXCLUDED.major_id, current_semester = EXCLUDED.current_semester WHERE users.azure_id = EXCLUDED.azure_id;
-        SELECT users.name, majors.name, users.current_semester FROM users LEFT JOIN majors ON majors.major_id = users.major_id WHERE azure_id = $4`
-        const result = await client.query(query, [name, m_id, curr_sem, a_id]);
+        const query1 = `INSERT INTO users (name, major_id, current_semester, azure_id) VALUES ($1, $2, $3, $4) ON CONFLICT (azure_id) DO UPDATE SET name = EXCLUDED.name, major_id = EXCLUDED.major_id, current_semester = EXCLUDED.current_semester WHERE users.azure_id = EXCLUDED.azure_id`
+        const query2 = `SELECT users.name, majors.name, users.current_semester FROM users LEFT JOIN majors ON majors.major_id = users.major_id WHERE azure_id = $1`
+        const result1 = await client.query(query1, [name, m_id, curr_sem, a_id]);
+        const result2 = await client.query(query2, [a_id]);
         context.res = {
             status: 200,
-            body: result.rows[0] || {},
+            body: result2.rows[0] || {},
         };
     } catch (error) {
         context.log('Error connecting to the database:', error);
